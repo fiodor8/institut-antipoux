f29c5cdc37e48013917af8942047284dc93de24a
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2023-04-12 17:42:53 +0100

    update site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://fiodor8.github.io/',
+  base: 'institut-antipoux/'
 });
